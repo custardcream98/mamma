26e0209fe514592131fa437ac9013a2fea202e55
shiwoo.park <custardcream@kakao.com>
2023-03-13 14:08:34 +0900

    Init project

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      pc: "768px"
+    },
+    extend: {}
+  },
+  plugins: []
+};
