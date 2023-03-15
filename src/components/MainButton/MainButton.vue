999e43967d3e3134fe2b2f556ad86ecad965705c
shiwoo.park <custardcream@kakao.com>
2023-03-15 17:37:33 +0900

    Style: MainButton 마진 변경

--- src/components/MainButton/MainButton.vue
+++ src/components/MainButton/MainButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <section text-36px font-300 mt-100px>
+  <section text-36px font-300 mt-100px mb-16px>
     <h2 sr-only>메인 디스플레이 영역</h2>
     <button text-left @click.stop="$emit('click')">
       <span block><slot name="first-line" /></span>
