0509e004dcbbc91a5b1036045a0cc15dfc8a66da
shiwoo.park <custardcream@kakao.com>
2023-03-15 09:49:15 +0900

    Config: unocss TailwindCSS 스타일로 설정

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,9 +1,15 @@
+import presetWind from "@unocss/preset-wind";
 import type { VitePluginConfig } from "@unocss/vite";
 import { presetAttributify, presetUno } from "unocss";
 
 const unocssConfig: VitePluginConfig = {
   include: [/\.variants\.[jt]s$/u, /\.[jt]sx?$/u, /\.vue$/u],
-  presets: [presetUno(), presetAttributify()],
+  presets: [presetUno(), presetAttributify(), presetWind()],
+  theme: {
+    breakpoints: {
+      pc: "448px",
+    },
+  },
 };
 
 export default unocssConfig;
