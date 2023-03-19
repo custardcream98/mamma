c3a3a1c04c4e365cd16dfc4db72962a65211ddda
shiwoo.park <custardcream@kakao.com>
2023-03-19 21:59:20 +0900

    Fix: 상호 없는 경우 에러나는 issue

--- src/request/get-restaurants-data.ts
+++ src/request/get-restaurants-data.ts
@@ -5,13 +5,15 @@ import { getSheetData } from "./get-sheet-data";
 const getRestaurantsData = async () => {
   const sheetResponse = await getSheetData();
 
-  const restaurants = sheetResponse.results.map((data) => {
-    if (!isRestaurantRawData(data)) {
-      throw new Error("Invalid data from Google Sheet");
-    }
+  const restaurants = sheetResponse.results
+    .filter((data) => "상호" in data && data["상호"])
+    .map((data) => {
+      if (!isRestaurantRawData(data)) {
+        throw new Error("Invalid data from Google Sheet");
+      }
 
-    return new Restaurant(data);
-  });
+      return new Restaurant(data);
+    });
 
   return restaurants;
 };
