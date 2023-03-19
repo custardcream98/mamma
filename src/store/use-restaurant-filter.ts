781e4d74f471a232d541b5e4a7d43d15dfbd78f6
shiwoo.park <custardcream@kakao.com>
2023-03-19 20:19:44 +0900

    Feat: filter 선택 정렬

--- src/store/use-restaurant-filter.ts
+++ src/store/use-restaurant-filter.ts
@@ -6,11 +6,22 @@ import { computed, ref } from "vue";
 export const useRestaurantFilterStore = defineStore("restaurant-filter", () => {
   const filter = ref<RestaurantType[]>([...RESTAURANT_TYPES]);
 
+  const sortFilter = () => {
+    filter.value.sort((a, b) => {
+      const aIndex = RESTAURANT_TYPES.indexOf(a);
+      const bIndex = RESTAURANT_TYPES.indexOf(b);
+
+      return aIndex - bIndex;
+    });
+  };
+
   const addFilter = (type: RestaurantType) => {
     filter.value.push(type);
+    sortFilter();
   };
   const removeFilter = (type: RestaurantType) => {
     filter.value = filter.value.filter((t) => t !== type);
+    sortFilter();
   };
   const filterText = computed(() => {
     if (filter.value.length === RESTAURANT_TYPES.length) {
