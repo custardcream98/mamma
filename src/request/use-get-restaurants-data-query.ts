ae78eedda3105c03be339a2ea1f122e226b29de3
shiwoo.park <custardcream@kakao.com>
2023-03-19 21:18:08 +0900

    Fix: useQuery stale time 설정

--- src/request/use-get-restaurants-data-query.ts
+++ src/request/use-get-restaurants-data-query.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/vue-query";
 import getRestaurantsData from "./get-restaurants-data";
 
+const queryKey = ["restaurants"];
+
 export const useGetRestaurantsDataQuery = () => {
-  return useQuery({
-    queryKey: ["restaurants"],
-    queryFn: getRestaurantsData,
+  return useQuery(queryKey, getRestaurantsData, {
+    staleTime: 1000 * 60 * 60 * 24,
   });
 };
