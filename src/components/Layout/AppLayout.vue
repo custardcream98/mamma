25e256de900f82e99ce4c3152be8412a6fff863e
shiwoo.park <custardcream@kakao.com>
2023-03-19 14:49:17 +0900

    Chore: AppFooter 추가

--- src/components/Layout/AppLayout.vue
+++ src/components/Layout/AppLayout.vue
@@ -1,24 +1,27 @@
 <script lang="ts" setup>
+import AppFooter from "./AppFooter.vue";
 import AppHeader from "./AppHeader.vue";
 </script>
 <template>
-  <div flex items-center h-100vh>
-    <div
-      m-auto
-      h-100vh
-      w-420px
-      min-h-500px
-      max-w-md
-      p-24px
-      pc:w-100vw
-      pc:h-95vh
-      pc:rounded-3xl
-      pc:shadow
-    >
-      <AppHeader />
-      <main>
-        <slot />
-      </main>
-    </div>
+  <div
+    flex
+    flex-col
+    m-auto
+    h-100vh
+    w-420px
+    min-h-500px
+    max-w-md
+    p-24px
+    pc:mt-2.5vh
+    pc:w-100vw
+    pc:h-95vh
+    pc:rounded-3xl
+    pc:shadow
+  >
+    <AppHeader />
+    <main flex-1>
+      <slot />
+    </main>
+    <AppFooter />
   </div>
 </template>
