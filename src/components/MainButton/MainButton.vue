f2cb9ce1b59eeb6bf83546645039a0b817cd4db0
shiwoo.park <custardcream@kakao.com>
2023-03-25 15:01:31 +0900

    Chore: 버튼 너비 수정 resolves #1

--- src/components/MainButton/MainButton.vue
+++ src/components/MainButton/MainButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button text-left text-36px font-300 mt-50px mb-16px>
+  <button text-left text-36px font-300 mt-50px mb-16px w-full>
     <span block><slot name="first-line" /></span>
     <span block><slot name="second-line" /></span>
   </button>
