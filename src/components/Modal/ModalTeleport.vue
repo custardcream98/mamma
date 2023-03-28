87a4463c0a08704ad7a4a0b63c6fe9ba3104e70e
shiwoo.park <custardcream@kakao.com>
2023-03-29 02:39:02 +0900

    Feat: 모달 backdrop 및 teleport 컴포넌트

---
+++ src/components/Modal/ModalTeleport.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const mounted = ref<boolean>(false);
+onMounted(() => {
+  mounted.value = true;
+});
+</script>
+
+<template>
+  <Teleport v-if="mounted" to="#teleport">
+    <slot></slot>
+  </Teleport>
+</template>
