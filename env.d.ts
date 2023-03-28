4e787314946f6fb3df73c860d73e66e232082b3c
shiwoo.park <custardcream@kakao.com>
2023-03-29 02:37:00 +0900

    Config: firebase 셋업

--- env.d.ts
+++ env.d.ts
@@ -3,5 +3,11 @@
 interface ImportMeta {
   env: {
     PROD: boolean;
+    VITE_FIREBASE_API_KEY: string;
+    VITE_FIREBASE_AUTH_DOMAIN: string;
+    VITE_FIREBASE_PROJECT_ID: string;
+    VITE_FIREBASE_STORAGE_BUCKET: string;
+    VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+    VITE_FIREBASE_APP_ID: string;
   };
 }
