99af34f02ca74743bc74d81f29f9f190e0295612
shiwoo.park <custardcream@kakao.com>
2023-03-31 23:31:20 +0900

    Fix: MainButton hover시 너비 이슈

--- src/components/Button/MainButton/MainButton.vue
+++ src/components/Button/MainButton/MainButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="text-left text-36px font-300 mt-50px mb-16px w-full hover:mx-[-10px] hover:mt-40px hover:mb-6px hover:p-10px hover:bg-wavveBlue_100 rounded-2xl"
+    class="text-left text-36px font-300 mt-50px mb-16px w-full hover:bg-wavveBlue_100 rounded-2xl hover:px-10px hover:ml-[-10px] box-content"
   >
     <slot name="first-line" />
     <slot name="second-line" />
