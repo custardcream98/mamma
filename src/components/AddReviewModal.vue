f3a6dda0a3a3d1e0bef6e1bfa027db02ee61f80f
shiwoo.park <custardcream@kakao.com>
2023-03-31 21:49:43 +0900

    Chore: 레스토랑 리스트에서 clickable 영역 변경

--- src/components/AddReviewModal.vue
+++ src/components/AddReviewModal.vue
@@ -2,14 +2,14 @@
 import { ModalBody } from "@/components/Modal";
 import { ModalWrapper } from "./Modal";
 import ModalClosseButton from "./Modal/ModalClosseButton.vue";
-import RestaurantsTable from "./RestaurantsTable.vue";
+import RestaurantsList from "./RestaurantsList.vue";
 </script>
 
 <template>
   <ModalWrapper>
     <ModalBody relative>
       <ModalClosseButton top-15px right-15px absolute />
-      <RestaurantsTable />
+      <RestaurantsList />
     </ModalBody>
   </ModalWrapper>
 </template>
