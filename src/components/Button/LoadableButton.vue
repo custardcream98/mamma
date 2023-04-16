1c485c5841a16be64a20834ff554b08c713553f8
shiwoo.park <custardcream@kakao.com>
2023-04-16 13:36:32 +0900

    Refactor: LoadingSpinner 컴포넌트 분리

--- src/components/Button/LoadableButton.vue
+++ src/components/Button/LoadableButton.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import LoadingSpinner from "../LoadingSpinner.vue";
+
 defineProps<{
   isLoading: boolean;
   warning?: boolean;
@@ -8,64 +10,6 @@ defineProps<{
 <template>
   <button :disabled="isLoading">
     <slot v-if="!isLoading"></slot>
-    <div
-      class="lds-ring"
-      :class="{
-        warning,
-      }"
-      v-else
-    >
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
+    <LoadingSpinner v-else :warning="warning" />
   </button>
 </template>
-
-<style scoped>
-.lds-ring {
-  display: inline-block;
-  position: relative;
-  width: 30px;
-  height: 30px;
-}
-.lds-ring div {
-  box-sizing: border-box;
-  display: block;
-  position: absolute;
-  top: 4px;
-  width: 21px;
-  height: 21px;
-  margin: 4px;
-  border: 4px solid #fff;
-  border-radius: 50%;
-  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-  border-color: #fff transparent transparent transparent;
-}
-
-.warning div {
-  border: 4px solid rgb(248, 113, 113);
-  border-color: rgb(248, 113, 113) transparent transparent transparent;
-  width: 20px;
-  height: 20px;
-  top: -4px;
-}
-.lds-ring div:nth-child(1) {
-  animation-delay: -0.45s;
-}
-.lds-ring div:nth-child(2) {
-  animation-delay: -0.3s;
-}
-.lds-ring div:nth-child(3) {
-  animation-delay: -0.15s;
-}
-@keyframes lds-ring {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-</style>
---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+defineProps<{
+  warning?: boolean;
+  blue?: boolean;
+}>();
+</script>
+
+<template>
+  <div
+    class="lds-ring"
+    :class="{
+      warning,
+      blue,
+    }"
+  >
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<style scoped>
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 30px;
+  height: 30px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  top: 4px;
+  width: 21px;
+  height: 21px;
+  margin: 4px;
+  border: 4px solid #fff;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #fff transparent transparent transparent;
+}
+
+.warning div {
+  border: 4px solid rgb(248, 113, 113);
+  border-color: rgb(248, 113, 113) transparent transparent transparent;
+  width: 20px;
+  height: 20px;
+  top: -4px;
+}
+
+.blue {
+  width: 16px;
+  height: 16px;
+}
+.blue div {
+  border-color: rgb(0, 80, 255) transparent transparent transparent;
+  width: 20px;
+  height: 20px;
+  top: -4px;
+  left: -8px;
+}
+
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
