0604af0ecda6696a2ce1c95463b175f133d74d91
da-in <talown@naver.com>
2023-04-17 13:01:30 +0900

    style/ update ui

--- src/request/axios.ts
+++ src/request/axios.ts
@@ -1,9 +1,7 @@
 import { AXIOS_CONSTANTS } from "@/constants/request";
 import axios from "axios";
 
-const BASE_URL = import.meta.env.PROD
-  ? AXIOS_CONSTANTS.BASE_URL
-  : AXIOS_CONSTANTS.DEV_URL;
+const BASE_URL = AXIOS_CONSTANTS.BASE_URL;
 
 const googleSheetApi = axios.create({
   baseURL: BASE_URL,
