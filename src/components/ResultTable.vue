9a802334acdd71e2d081a5d9304b7aad12e5cbd8
shiwoo.park <custardcream@kakao.com>
2023-04-20 22:56:05 +0900

    Refactor: 필요없는 유틸 제거

--- src/components/ResultTable.vue
+++ src/components/ResultTable.vue
@@ -1,34 +1,33 @@
 <script lang="ts" setup>
 import { useRestaurantMetaData } from "@/composables";
 import { numberToFormattedString } from "@/utils/format";
-import { withDefaultString } from "@/utils/string";
 
 const { restaurantMetaData } = useRestaurantMetaData();
 </script>
 
 <template>
-  <table mt-16px v-if="restaurantMetaData">
+  <table mt-16px>
     <tr>
       <th>카테고리</th>
-      <td>{{ withDefaultString(restaurantMetaData.type, "-") }}</td>
+      <td>{{ restaurantMetaData?.type ?? "-" }}</td>
     </tr>
     <tr>
       <th>대표메뉴</th>
-      <td>{{ withDefaultString(restaurantMetaData.menu, "-") }}</td>
+      <td>{{ restaurantMetaData?.menu ?? "-" }}</td>
     </tr>
     <tr>
       <th>가격</th>
       <td>
         {{
-          restaurantMetaData.price
-            ? `인당 약 ${numberToFormattedString(restaurantMetaData.price)}원`
+          restaurantMetaData?.price
+            ? `인당 약 ${numberToFormattedString(restaurantMetaData?.price)}원`
             : "-"
         }}
       </td>
     </tr>
     <tr>
       <th>위치</th>
-      <td>{{ withDefaultString(restaurantMetaData.location, "-") }}</td>
+      <td>{{ restaurantMetaData?.location ?? "-" }}</td>
     </tr>
   </table>
 </template>
--- src/utils/string.ts
+++
@@ -1,5 +0,0 @@
-const withDefaultString = (value: string | undefined, defaultValue: string) => {
-  return value ? value : defaultValue;
-};
-
-export { withDefaultString };
